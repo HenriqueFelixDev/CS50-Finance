13073d42f3b42a5f1e14240ddf25e2e7b43dfcf6
Henrique Felix <henriquefelixdev@gmail.com>
2023-11-21 17:24:48 -0300

    feat: create default layout and signup page

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import core.filters as filters
 
 app = Flask(__name__)
@@ -7,9 +7,9 @@ app.jinja_env.filters['usd'] = filters.usd
 app.jinja_env.filters['date'] = filters.date
 app.jinja_env.filters['datetime'] = filters.datetime
 
-@app.route('/')
+@app.route('/signup', methods=['GET'])
 def index():
-  return 'OK'
+  return render_template('pages/signup.html')
 
 if __name__ == '__main__':
   app.run()
