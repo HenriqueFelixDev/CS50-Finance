f1f22e307b4cdb34e15fae3ed61b2cdec9124482
Henrique Felix <henriquefelixdev@gmail.com>
2023-11-21 15:07:58 -0300

    feat: create jinja custom filters

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import core.filters as filters
 
 app = Flask(__name__)
+
+app.jinja_env.filters['usd'] = filters.usd
+app.jinja_env.filters['date'] = filters.date
+app.jinja_env.filters['datetime'] = filters.datetime
 
 @app.route('/')
 def index():
