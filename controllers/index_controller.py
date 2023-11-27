df67bd9f7c98038a4e642c50ce67f463a8c5623e
Henrique Felix <henriquefelixdev@gmail.com>
2023-11-27 13:20:33 -0300

    fix: getStockPrices query only when actions list is not empty

--- controllers/index_controller.py
+++ controllers/index_controller.py
@@ -6,11 +6,12 @@ from entities.transaction import ActionCreateDTO
 def getIndexPage():
   actions = transactions_repository.getActiveActions(userId=session['id'])
 
-  symbols = map(lambda action: action.symbol, actions)
-  quotes = quotes_repository.getStockPrices(symbols)
+  if (len(actions) > 0):
+    symbols = map(lambda action: action.symbol, actions)
+    quotes = quotes_repository.getStockPrices(symbols)
 
-  for action in actions:
-    action.price = quotes[action.symbol]['price']
+    for action in actions:
+      action.price = quotes[action.symbol]['price']
 
   return render_template('pages/index.html', actions=actions)
 
